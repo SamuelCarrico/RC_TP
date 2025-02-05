bae556ba71181a7b796df72fe216f8049fc8f9f1
Samuel Carrico <samuel.carrico@viacesi.fr>
2025-02-05 17:03:20 +0100

    feat: init

---
+++ tp/app/_layout.tsx
@@ -0,0 +1,23 @@
+import {tokenCache} from '../utils/cache'
+import {ClerkProvider, ClerkLoaded} from '@clerk/clerk-expo'
+import {Slot} from 'expo-router'
+import {PaperProvider} from "react-native-paper";
+
+
+export default function RootLayout() {
+    const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+    if (!publishableKey) {
+        throw new Error('Add EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env')
+    }
+
+    return (
+        <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+            <ClerkLoaded>
+                <PaperProvider>
+                    <Slot/>
+                </PaperProvider>
+            </ClerkLoaded>
+        </ClerkProvider>
+    )
+}
---
+++ tp/styles/styles.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        margin: 20,
+        backgroundColor: "#F5F7FA",
+        padding: 20,
+        justifyContent: "center",
+    },
+});
